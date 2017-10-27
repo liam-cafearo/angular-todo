b2772ef63baf533ab9db0334082cf23bdb750945
liam-cafearo <lcafearo@gmail.com>
2017-10-27 21:50:01 +0100

    rectified mistakes in app.js file

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 angular.module('TodoApp', ['ngRoute', 'RouteControllers']);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
-	$locationProvider.html5mode(true); // Enable href routing without hashes
+	$locationProvider.html5Mode(true); // Enable href routing without hashes
 
 	$routeProvider.when('/', {
 		templateUrl: 'templates/home.html',
 		controller: 'HomeController'
 	})
-	.when('/accounts/register' {
+	.when('/accounts/register', {
 		templateUrl: 'templates/register.html',
 		controller: 'RegisterController'
 	});
