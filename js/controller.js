307ee2dcff21fbd97ee36bc79da7d522189c92f3
liam-cafearo <lcafearo@gmail.com>
2017-10-29 17:43:16 +0000

    added files for JWT authenticating against REST API

--- js/controller.js
+++ js/controller.js
@@ -3,19 +3,14 @@ angular.module('RouteControllers', [])
 		$scope.title = "Welcome To Angular Todo!";
 	})
 	.controller('RegisterController', function($scope, UserAPIService, store) {
-
 		$scope.registrationUser = {};
 		var URL = "https://morning-castle-91468.herokuapp.com/";
-
-		var authStorage = {
-			name: "storageTest"
-		};
-
-		store.set('obj', authStorage);
 
 		$scope.login = function() {
 			UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
 				$scope.token = results.data.token;
+				store.set('username', $scope.registrationUser.username);
+				store.set('authToken', $scope.token);
 				console.log($scope.token);
 			}).catch(function(err) {
 				console.log(err.data);
