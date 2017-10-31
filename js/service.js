dbdae93c5c4def1108fc58d3407ed3c89a2d1d84
liam-cafearo <lcafearo@gmail.com>
2017-10-31 00:10:55 +0000

    added edit and delete functions

--- js/service.js
+++ js/service.js
@@ -19,7 +19,15 @@ angular.module('TodoService', [])
 			createTodo: function(url, data, token) {
 				var header = "Authorization: JWT " + token;
 				return $http.post(url, data, header);
+			},
+			editTodo: function(url, data, token) {
+				var header = "Authorization: JWT " + token;
+				return $http.put(url, data, header);
+			},
+			deleteTodo function(url, token) {
+				var header = "Authorization: JWT " + token;
+				return $http.delete(url, token);
 			}
-		};
+		}
 		return TodoAPIService;
 	});
